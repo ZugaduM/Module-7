4c8ac7896cabc9d981254989f96d70080b8c3d86
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-06-13 00:07:41 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,3 +1,3 @@
 file_name = "text_file.txt"
-with open(file_name, "r") as file:
+with open(file_name, "r") as file: # оператор with практичнее для использования, так как автоматически закрывает файл после завершения обращения к нему
     print(file.read())
